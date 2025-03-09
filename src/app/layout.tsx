86a94a5e60ec8d99a3ec4982f1cc872abf1f33e2
Pranta Dutta <prantadutta1997@gmail.com>
2025-03-09 20:11:06 +0600

    removed firebase

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,7 +11,6 @@ import { Footer } from '@/components/footer'
 import { ThemeSwitch } from '@/components/theme-switch'
 import { ThemeContextProvider } from '@/context/theme-context'
 import { SpeedInsights } from '@vercel/speed-insights/next'
-import TriggerAnalytics from '@/components/TriggerAnalytics'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -28,13 +27,13 @@ const RootLayout: React.FC<IRootLayoutProps> = ({ children }) => {
   return (
     <html lang='en' className='!scroll-smooth'>
       <body
-        className={`${inter.className} dark:text-opacity-90 relative bg-gray-50 pt-28 text-gray-950 sm:pt-36 dark:bg-gray-900 dark:text-gray-50`}
+        className={`${inter.className} relative bg-gray-50 pt-28 text-gray-950 dark:bg-gray-900 dark:text-gray-50 dark:text-opacity-90 sm:pt-36`}
       >
         {/* <div className='special-border absolute right-[11rem] top-[-6rem] -z-10 h-[31.25rem] w-[31.25rem] bg-[#fbe2e3] blur-[10rem] dark:bg-[#946263] sm:w-[68.75rem]' />
         <div className='special-border absolute left-[-35rem] top-[-1rem] -z-10 h-[31.25rem] w-[50rem] bg-[#dbd7fb] blur-[10rem] dark:bg-[#676394] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem]' /> */}
 
-        <div className='special-border fixed top-[-6rem] right-[11rem] -z-10 h-[31.25rem] w-[31.25rem] bg-[#6ed0ac] blur-[10rem] sm:w-[68.75rem] dark:bg-[#204f57]' />
-        <div className='special-border fixed top-[-1rem] left-[-35rem] -z-10 h-[31.25rem] w-[50rem] bg-[#9bd2d3] blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem] dark:bg-[#3e7055]' />
+        <div className='special-border fixed right-[11rem] top-[-6rem] -z-10 h-[31.25rem] w-[31.25rem] bg-[#6ed0ac] blur-[10rem] dark:bg-[#204f57] sm:w-[68.75rem]' />
+        <div className='special-border fixed left-[-35rem] top-[-1rem] -z-10 h-[31.25rem] w-[50rem] bg-[#9bd2d3] blur-[10rem] dark:bg-[#3e7055] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem]' />
 
         <ThemeContextProvider>
           <ActiveSectionContextProvider>
@@ -47,7 +46,6 @@ const RootLayout: React.FC<IRootLayoutProps> = ({ children }) => {
         </ThemeContextProvider>
         <Analytics />
         <SpeedInsights />
-        <TriggerAnalytics />
       </body>
     </html>
   )
