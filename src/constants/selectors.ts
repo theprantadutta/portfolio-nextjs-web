ae6d5699ec922bf7b8b450082f9597ddcae98306
PRANTA Dutta <prantadutta1997@gmail.com>
2025-05-26 22:19:47 +0600

    updated cv path again

--- src/constants/selectors.ts
+++ src/constants/selectors.ts
@@ -1,4 +1,4 @@
-export const CV_PATH = '/PRANTA_CV_MAY_2025_NO.pdf'
+export const CV_PATH = '/PRANTA_CV_JUNE_2025_NO.pdf'
 
 export const LINKED_IN_LINK = 'https://www.linkedin.com/in/theprantadutta/'
 
